2bc94a0431483490a97387a3a2f51fac573b4b7a
Vera <148436811+szatanenjoyer666@users.noreply.github.com>
2025-10-06 10:08:00 +0200

    zad 2 dok.

---
+++ zadanie.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout << "Podaj baze (a): ";
+   cin >> a;
+cout << "Podaj wykladnik (n): "
+   cin >> n;
+wynik = 
+while ( n > 0 ) {
+    if (n % 2 == 1){
+        wynik = wynik * a;
+        else{
+            wynik = 
+        }
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dlaczego zmienne przed funkcja
