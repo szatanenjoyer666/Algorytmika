f75b1c897e9b984a35bf2412c656c9bba209422f
Vera <148436811+szatanenjoyer666@users.noreply.github.com>
2025-11-26 23:33:15 +0100

    zadań2.cpp

--- zadań2.cpp
+++ zadań2.cpp
@@ -1,24 +1,22 @@
 #include<iostream>
 using namespace std;
-// Rozwiązanie iteracyjne
-int pot_szybkie(int baza, int wyklad)
+
+int pot_szybkie(int baza, int wyklad) //param
 {
-	int wynik = 1;
+	int wynik = 1; //inicjalizacja, start
 	
-	while(wyklad>0)
-	{
-		if (wyklad%2 == 1) 
-			wynik *= baza;
+	while(wyklad>0) {
+		if (wyklad%2 == 1) //reszka /
+			wynik *= baza; 
 			
-		baza*= baza;
+		baza*= baza; //podwojenie pot
 		wyklad/=2; 
 	}
-	return wynik;
+	return wynik; 
 }
 
-int main()
-{
-	int wyklad;
+int main(){
+	int wyklad; //zm
 	int baza;
 	
 	cout<<"Podaj podstawę: ";
@@ -26,7 +24,8 @@ int main()
 	cout<<"Podaj wykładnik: ";
 	cin>>wyklad;
 	
-	cout<<baza<<" do potęgi "<<wyklad<<" wynosi "<<pot_szybkie(baza, wyklad);
+	cout<<baza<<" do potęgi "<<wyklad<<" wynosi "<<pot_szybkie(baza, wyklad); //main nic nie widzi nic nie słyszy
 
 	return 0;
+
 }
