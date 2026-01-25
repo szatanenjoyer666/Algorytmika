798cd6f2360bdb47f9c7a73f5550ede9926ca84d
Vera <148436811+szatanenjoyer666@users.noreply.github.com>
2026-01-25 18:25:23 +0100

    Update zadań5.cpp

--- zadań5.cpp
+++ zadań5.cpp
@@ -4,15 +4,15 @@ using namespace std;
 void zamianka(int liczba)
 {
     int tab[31];
-    int i = 0;
+    int i = 0; //licz pozy tab
 
 
     while (liczba != 0)
     {
-        tab[i++] = liczba % 2;
+        tab[i++] = liczba % 2; //zapisujesz resztę z dzielenia przez 2, zwiększasz i
         liczba /= 2;
     }
-    for (int j = i - 1; j >= 0; j--)
+    for (int j = i - 1; j >= 0; j--) //wypisuje lelementy tablicy od ostatniego do pierwszwego
     {
         cout << tab[j];
     }
